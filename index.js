d3d49cde15c4ed33466f0184d0ff3bf07ab7588c
Noel Bryant <noelsobryant@gmail.com>
2023-08-23 02:32:18 +0300

    refactor code to use arrow functions

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ import BookManager from './modules/BookManager.js';
 import GetTime from './modules/GetTime.js';
 import navHandler from './modules/NavigationHandler.js';
 
-function initializeApp() {
+const initializeApp = () => {
   const app = new BookManager();
 
   document.getElementById('form-section').style.display = 'none';
   document.getElementById('contact-section').style.display = 'none';
 
   navHandler();
-  app.navHandler();
   return app;
-}
+};
 
 GetTime();
 
